590ff604cbb06ab1b3fee726b27829caa872a531
danny <dannytakushi@gmail.com>
2024-11-20 17:27:32 -0600

    data_module_base

---
+++ data_module_base/include/data_module_base.h
@@ -0,0 +1,20 @@
+#pragma once
+#include "connection_factory.h"
+
+class data_module_base{
+ protected:
+  connection_type connection_type_;
+  std::shared_ptr<connection_i> local_conn_;
+  bool is_active_{false};
+  std::thread work_loop_thread_;
+  virtual void work_loop() = 0;
+  void local_publish(std::string topic, std::string data);
+
+ public:
+  data_module_base(connection_type conn_type);
+  virtual void setup() = 0;
+  void start_work_loop();
+  virtual void close() = 0;
+
+  bool is_active();
+};
---
+++ data_module_base/src/data_module_base.cpp
@@ -0,0 +1,17 @@
+#include  "data_module_base.h"
+data_module_base::data_module_base(connection_type conn_type) :
+    connection_type_(conn_type){}
+
+void data_module_base::start_work_loop(){
+  is_active_=true;
+  work_loop_thread_ = std::thread([this](){
+    this->work_loop();
+  });
+}
+void data_module_base::local_publish(std::string topic, std::string data){
+  local_conn_->publish(topic,data);
+}
+
+bool data_module_base::is_active(){
+  return is_active_;
+}
