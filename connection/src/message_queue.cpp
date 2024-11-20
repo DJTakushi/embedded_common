b00449da4ca146f8c977efe5f15d63678a0d8344
danny <dannytakushi@gmail.com>
2024-11-19 20:21:37 -0600

    correct debugging

--- connection/src/message_queue.cpp
+++ connection/src/message_queue.cpp
@@ -8,7 +8,6 @@ std::string message_queue::get_popped_message(){
   std::lock_guard<std::mutex> lock(message_mutex);
   std::string out = "";
   if(messages_.size()>0){
-    std::cout << "messages_ popping excess" << std::endl;
     out = messages_.front();
     messages_.pop();
   }
@@ -18,6 +17,7 @@ std::string message_queue::get_popped_message(){
 void message_queue::add_message_to_queue(std::string msg){
   std::lock_guard<std::mutex> lock(message_mutex);
   while(messages_.size() > max_messages_){
+    std::cout << "messages_ popping excess" << std::endl;
     messages_.pop();
   }
   messages_.push(msg);
