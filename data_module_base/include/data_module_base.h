8b70cdc8c4cbdad46af5a29701667523df55ffdb
danny <dannytakushi@gmail.com>
2024-11-22 12:13:46 -0600

    data_module_base : publish_key, data threads

--- data_module_base/include/data_module_base.h
+++ data_module_base/include/data_module_base.h
@@ -3,16 +3,29 @@
 
 class data_module_base{
  protected:
+  std::string publish_key_;
   connection_type connection_type_;
   std::shared_ptr<connection_i> local_conn_;
   bool is_active_{false};
   std::thread work_loop_thread_;
   virtual void work_loop() = 0;
+
+  std::thread receive_data_thread_;
+  void receive_data_loop();
+  virtual void receive_data() = 0;
+
+  std::thread update_data_thread_; // update attributes & publish
+  void update_data_loop();
+  virtual void update_data() = 0;
+
+  void publish_data();
+  std::string generate_update_message();
+
   void setup_local_conn();
   void local_publish(std::string topic, std::string data);
 
  public:
-  data_module_base(connection_type conn_type);
+  data_module_base(std::string pub_key, connection_type conn_type);
   virtual void setup() = 0;
   void start_work_loop();
   virtual void close() = 0;
--- data_module_base/src/data_module_base.cpp
+++ data_module_base/src/data_module_base.cpp
@@ -1,5 +1,6 @@
 #include  "data_module_base.h"
-data_module_base::data_module_base(connection_type conn_type) :
+data_module_base::data_module_base(std::string pub_key, connection_type conn_type) :
+    publish_key_(pub_key),
     connection_type_(conn_type){}
 
 void data_module_base::setup_local_conn(){
@@ -19,4 +20,28 @@ void data_module_base::local_publish(std::string topic, std::string data){
 
 bool data_module_base::is_active(){
   return is_active_;
+}
+
+void data_module_base::receive_data_loop(){
+  while(is_active_){
+    receive_data();
+  }
+}
+
+void data_module_base::update_data_loop(){
+  while(is_active_){
+    update_data();
+  }
+}
+
+void data_module_base::publish_data(){
+  // TODO : check if data is worth updating
+  std::string msg =  generate_update_message();
+  local_publish(publish_key_, msg);
+}
+
+std::string data_module_base::generate_update_message(){
+  std::string out;
+  // TODO
+  return out;
 }
