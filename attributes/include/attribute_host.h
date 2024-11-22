05c30f6a0244218d2908348180f62d97e9d7ad6a
danny <dannytakushi@gmail.com>
2024-11-22 10:18:23 -0600

    attribute_host::get_updated_attributes() replaces ddata_gen()

--- attributes/include/attribute_host.h
+++ attributes/include/attribute_host.h
@@ -8,6 +8,7 @@
 #include "attribute.h"
 
 typedef std::map<std::string,std::shared_ptr<attribute>> attribute_map;
+typedef std::vector<std::shared_ptr<attribute>> attribute_vector;
 class attribute_host{
   attribute_map attributes_;
  public:
@@ -15,5 +16,5 @@ class attribute_host{
   std::mutex attribute_mutex;
   void update_attribute(nlohmann::json& j, steady_tp time);
   void update_attributes_from_array(nlohmann::json& j);
-  void ddata_gen(org_eclipse_tahu_protobuf_Payload* payload);
+  attribute_vector get_updated_attributes();
 };
--- attributes/src/attribute_host.cpp
+++ attributes/src/attribute_host.cpp
@@ -10,7 +10,8 @@ void attribute_host::update_attribute(nlohmann::json& j, steady_tp time){
     uint64_t attr_datatype = j["datatype"];
     if(attributes_.find(attr_name)==attributes_.end()){
       std::cout << "creating attribute : "<< attr_name <<"..."<<std::endl;
-      attributes_[attr_name] = std::make_shared<attribute>(attr_datatype);
+      attributes_[attr_name] = std::make_shared<attribute>(attr_name,
+                                                            attr_datatype);
     }
     else{
       // do nothing?
@@ -44,31 +45,12 @@ void attribute_host::update_attributes_from_array(nlohmann::json& j){
     std::cout << "json not an array : " << j.dump()<<std::endl;
   }
 }
-void attribute_host::ddata_gen(org_eclipse_tahu_protobuf_Payload* payload){
+attribute_vector attribute_host::get_updated_attributes(){
+  attribute_vector out;
   for(auto attr : attributes_){
     if(attr.second->is_recently_published()){
-      size_t attr_size;
-      switch (attr.second->get_datatype()){
-        case METRIC_DATA_TYPE_INT64:
-          attr_size = sizeof(uint64_t);
-          break;
-        case METRIC_DATA_TYPE_DOUBLE:
-          attr_size = sizeof(double);
-          break;
-        case METRIC_DATA_TYPE_STRING:
-          attr_size = sizeof(std::string);
-          break;
-      }
-
-      add_simple_metric(payload,
-                        attr.first.c_str(),
-                        true,
-                        0, // alias; should maybe be removed
-                        attr.second->get_datatype(),
-                        false,
-                        false,
-                        attr.second->get_value(),
-                        attr_size);
+      out.push_back(attr.second);
     }
   }
+  return out;
 }
