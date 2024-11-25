03ac119f8afbde8deed6b98997851e139ed864c9
danny <dannytakushi@gmail.com>
2024-11-25 08:26:12 -0600

    get_attributes_from_data() rename from get_data()

--- parser/include/parser_i.h
+++ parser/include/parser_i.h
@@ -4,7 +4,7 @@ class parser_i{
   virtual void configure(nlohmann::json config) = 0;
 
   /** generate data from pointer to data-source*/
-  virtual nlohmann::json get_data(void* data) = 0;
+  virtual nlohmann::json get_attributes_from_data(void* data) = 0;
 
   virtual nlohmann::json get_all_supported_attributes() = 0;
 
