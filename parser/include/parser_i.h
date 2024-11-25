23343495f3114186efa0b2f137fb11227f9b3502
danny <dannytakushi@gmail.com>
2024-11-25 08:48:56 -0600

    get_attributes_from_data() takes epoch as argument

--- parser/include/parser_i.h
+++ parser/include/parser_i.h
@@ -1,10 +1,12 @@
 #pragma once
 #include "nlohmann/json.hpp"
 class parser_i{
+ public:
   virtual void configure(nlohmann::json config) = 0;
 
   /** generate data from pointer to data-source*/
-  virtual nlohmann::json get_attributes_from_data(void* data) = 0;
+  virtual nlohmann::json get_attributes_from_data(void* data,
+                                                  uint64_t epoch) = 0;
 
   virtual nlohmann::json get_all_supported_attributes() = 0;
 
