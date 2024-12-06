eb0fafba0ebddd09137c0c3d573122f01463c7ed
danny <dannytakushi@gmail.com>
2024-12-06 11:06:38 -0600

    data_module_i simplification

--- data_module_base/include/data_module_i.h
+++ data_module_base/include/data_module_i.h
@@ -7,9 +7,8 @@ class data_module_i{
  public:
   data_module_i(){};
   virtual void setup() = 0;
-  virtual void start_all_threads() = 0;
-  virtual void stop_all_threads() = 0;
-  virtual void close() = 0;
-  virtual bool is_active() = 0;
+  virtual void start_running() = 0;
+  virtual void exit() = 0;
+  virtual bool is_exited() = 0;
 };
 }//ec
